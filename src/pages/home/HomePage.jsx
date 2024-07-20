d04f85d99e403b096715351a2211b160ff0201fd
Sebastián barrera <sebasbh2008@hotmail.com>
2024-07-19 20:39:24 -0500

    add folder pages

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import BodyHome from "../../components/Home/bodyHome";
+import NavBar from "../../components/navBar";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavBar/>
+      <BodyHome />
+    </>
+  );
+};
+
+export default HomePage;
